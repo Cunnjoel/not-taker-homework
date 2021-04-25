069472265256a4e2d3ee315f011912e15fd61059
Cunnjoel <cunningham.joel84@gmail.com>
2021-04-24 22:19:32 -0500

    updating js files

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-
 const express = require('express');
+const path = require('path')
+const fs = require("fs");
+
+const store = require('./Main/db/db.json')
 
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -15,17 +16,50 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+fs.readFile("Main/db/db.json", "utf8", (err, data) => {
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+  if (err) throw err;
+
+  const notes = JSON.parse(data);
+  //API get request
+  app.get('/api/notes', (req, res) => res.json(notes));
+
+  //HTML get requests
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Main/public/notes.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Main/public/index.html'));
+  });
+
+
+  //API post request
+  app.post('/api/notes', (req, res) => {
+
+    let newNote = req.body
+    notes.push(newNote);
+    updateNote();
+    return console.log("New note added: " + newNote.title);
+  });
+
+  //retrieve note
+  app.get("/api/notes/:id", function (req, res) {
+    res.json(notes[req.params.id]);
+  })
+});
+
+function updateNote() {
+  fs.writeFile("Main/db/db.json", JSON.stringify(notes, '/t'), err => {
+    if (err) throw err;
+    return true;
+  });
+}
 
 // LISTENER
 // The below code effectively "starts" our server
-
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
